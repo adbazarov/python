8c4f445fddb77ec5db73f572abed4b2ea8664389
adbazarov <adbazarov@yandex.ru>
2022-10-26 14:52:27 +0800

    Update adc.py

--- Raspberry/ADS1210/adc.py
+++ Raspberry/ADS1210/adc.py
@@ -1,9 +1,8 @@
 import spidev
 import RPi.GPIO as GPIO
 import time
-CS_PIN_ADC1 = 25
-DRDY_PIN = 8
 
+DRDY_PIN = 8
 RST_PIN = 7
 CS={
         'ADC1':25,
@@ -30,70 +29,86 @@ Calibration={
 
 class ADS1210:
     def __init__(self):
-        def self_test_adc():
-            reset_adc()
-            digital_write(CS_PIN, 0)
-            spi_writebyte(5)
-            spi_writebyte(32)
-            digital_write(CS_PIN, 1)
+        self.SPI = spidev.SpiDev(0, 0)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(RST_PIN, GPIO.OUT)
+        GPIO.setup(CS['ADC1'], GPIO.OUT)
+        GPIO.setup(DRDY['ADC1'], GPIO.IN)
+        self.SPI.max_speed_hz = 20000
+        self.SPI.mode = 0b01
+        self.SPI.no_cs = True
+
+    def self_test_adc(self):
+        self.reset_adc()
+        self.digital_write(CS['ADC1'], 0)
+        self.spi_writebyte(5)
+        self.spi_writebyte(32)
+        self.digital_write(CS['ADC1'], 1)
 
 
-        def wait_DRY():
-            for i in range(0, 40000, 1):
-                if (digital_read(DRDY_PIN) == 0):
-                    break
+    def wait_DRY(self):
+        for i in range(0, 40000, 1):
+            if (self.digital_read(DRDY['ADC1']) == 0):
+                break
             if (i >= 39000):
                 print('Time out wait DRDY!')
+                break
 
-        def init_adc():
-            CMD = [0b01000010,0x00, 0x00, 0x4D]
-            reset_adc()
-            digital_write(CS_PIN, 0)
-            wait_DRY()
-            spi_writebyte(WR_CMD)
-            delay_ms(1)
-            spi_writebyte(CMD[0])
-            spi_writebyte(CMD[1])
-            spi_writebyte(CMD[2])
-            spi_writebyte(CMD[3])
-            digital_write(CS_PIN, 1)
-            print(CMD)
+    def init_adc(self):
+        CMD = [0b01000010,0x00, 0x00, 0x4D]
+        self.reset_adc()
+        self.digital_write(CS['ADC1'], 0)
+        self.wait_DRY()
+        self.spi_writebyte(CMD['WR_CMD'])
+        self.delay_ms(1)
+        self.spi_writebyte(CMD[0])
+        self.spi_writebyte(CMD[1])
+        self.spi_writebyte(CMD[2])
+        self.spi_writebyte(CMD[3])
+        self.digital_write(CS['ADC1'], 1)
+        print(CMD)
+
+    def module_init(self ):
+        print('Init module')
+
+    def reset_adc(self ):
+        self.digital_write(RST_PIN, 0)
+        self.delay_ms(1)
+        self.digital_write(RST_PIN, 1)
+
+    def cs_adc(self, channel):
+        self.digital_write(CS['ADC1'], 0)
+
+    def digital_write(self, pin, value):
+        GPIO.output(pin, value)
+
+    def digital_read(self, pin):
+        return GPIO.input(pin)
+
+    def delay_ms(self, delaytime):
+        time.sleep(delaytime // 1000.)
+
+    def spi_writebyte(self, data):
+        self.SPI.writebytes([data])
+
+    def spi_readbyte(self, reg):
+        return self.SPI.readbytes(reg)
+    def read_data(self):
+        self.wait_DRY()
+        self.digital_write(CS['ADC1'], 0)
+        self.spi_writebyte(CMD['RD_DATA'])
+        self.delay_ms(1)
+        byte_1 = self.spi_readbyte(0x01)
+        byte_2 = self.spi_readbyte(0x01)
+        byte_3 = self.spi_readbyte(0x01)
+        self.digital_write(CS['ADC1'], 1)
+        print(byte_1, byte_2, byte_3, ((byte_1[0] << 16) | (byte_2[0] << 8) | (byte_3[0])) - 8388608)
+
 # end class ADS1210
 
 
-def digital_write(pin,value):
-    GPIO.output(pin,value)
 
-def digital_read(pin):
-    return GPIO.input(pin)
 
-def delay_ms(delaytime):
-    time.sleep(delaytime//1000.)
-
-def spi_writebyte(data):
-    SPI.writebytes([data])
-
-def spi_readbyte(reg):
-    return SPI.readbytes(reg)
-
-def module_init():
-
-    GPIO.setmode(GPIO.BCM)
-#    GPIO.setwarning(False)
-    GPIO.setup(RST_PIN,GPIO.OUT)
-    GPIO.setup(CS_PIN,GPIO.OUT)
-    GPIO.setup(DRDY_PIN,GPIO.IN)#pull_up_down=GPIO.PUD_UP
-    SPI.max_speed_hz=20000
-    SPI.mode=0b01
-    SPI.no_cs=True
-
-def reset_adc():
-    digital_write(RST_PIN,0)
-    delay_ms(1)
-    digital_write(RST_PIN,1)
-
-def cs_adc(channel):
-    digital_write(CS_PIN,0)
 
 
 
@@ -101,7 +116,7 @@ def cs_adc(channel):
 
 #======================================    
 print('start adc')
-SPI=spidev.SpiDev(0,0)
+
 module_init()
 init_adc()
 self_test_adc()
